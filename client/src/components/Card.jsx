818538db9aa5c8f9881b2784d04d6e645a57bc68
Sudhansu Sekhar Behera <118283474+skehargit@users.noreply.github.com>
2024-08-14 15:40:32 +0530

    update

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { apiRequest } from "../utils";
 import axios from "axios";
+import JobCard from "./JobCard";
 
 function Card({ jobId, name, location}) {
   const params = useParams();
@@ -19,7 +20,7 @@ function Card({ jobId, name, location}) {
       console.log(jobId)
       const res = await axios.get(`https://demojobportal.onrender.com/api-v1/jobs/get-job-detail/${jobId}`)
       console.log('card -',res)
-        console.log(res.data.data.jobTitle);
+      console.log(res.data.data.jobTitle);
       setInfo(res.data.data);
       
       setIsLoading(false);
@@ -35,45 +36,7 @@ function Card({ jobId, name, location}) {
   return (
     <>
       <Link to={`/job-detail/${jobId}`}>
-        <div
-        // h-[16rem] md:h-[18rem]
-          className="w-full md:w-[16rem] 2xl:w-[18rem]  bg-white flex flex-col justify-between shadow-lg 
-          rounded-md px-3 py-5 "
-        >
-          <div className="flex gap-3">
-            {/* <img
-              src={jobId?.company?.profileUrl}
-              alt={jobId?.company?.name}
-              className="w-14 h-14"
-            /> */}
-            <h1>{name}</h1>
-
-            <div className="">
-              <p className="text-sm font-semibold truncate">
-                {info&&info?.jobTitle.slice(0,25) + "..."}
-              </p>
-              <span className="flex gap-2 items-center">
-                <GoLocation className="text-slate-900 text-xs" />
-                {info&&info?.location}
-              </span>
-            </div>
-          </div>
-
-          <div className="py-3">
-            <p className="text-sm">
-              {info&&info?.detail[0]?.desc?.slice(0, 150) + "..."}
-            </p>
-          </div>
-
-          <div className="flex items-center justify-between">
-            <p className="bg-[#1d4fd826] text-[#1d4fd8] py-0.5 px-1.5 rounded font-semibold text-sm">
-              {info&&info?.jobType}
-            </p>
-            <span className="text-gray-500 text-sm">
-              {moment(info&&info?.createdAt).fromNow()}
-            </span>
-          </div>
-        </div>
+        <JobCard job={info}></JobCard>
       </Link>
     </>
   );
